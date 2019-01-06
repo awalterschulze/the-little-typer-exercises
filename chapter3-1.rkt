5b034bd4fabe95982f826b7ed773db6545b6db24
Walter Schulze <awalterschulze@gmail.com>
2019-01-06 17:49:13 +0000

    Create chapter3-1.rkt

---
+++ chapter3-1.rkt
@@ -0,0 +1,26 @@
+#lang pie
+
+;;; Define a function called at-least-two? that takes one Nat argument and evaluates to an Atom.
+;;; at-least-two? evaluates to 't if the Nat is greater than or equal to 2 otherwise it evaluates to 'nil.
+;;; Note:
+;;; The only Nat eliminator you should need in the body of at-least-two? is which-Nat.
+
+(claim at-least-two (-> Nat Atom))
+(define at-least-two
+  (lambda (n)
+    (which-Nat n
+      'nil
+      (lambda (n-1)
+        (which-Nat n-1
+          'nil
+          (lambda (n-2) 't)
+        )
+      )
+    )
+  )
+)
+
+(at-least-two 0)
+(at-least-two 1)
+(at-least-two 2)
+(at-least-two 3)
